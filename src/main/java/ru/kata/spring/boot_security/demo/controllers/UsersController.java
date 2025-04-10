718940386de5d10e7eeb19ebb7625577ef00009d
ZhulkovNikolay <wed10@yandex.ru>
2025-04-10 22:42:13 +0300

    попробовал @PreAuthorize на Админа

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UsersController.java
@@ -1,13 +1,22 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.security.PersonDetails;
+import ru.kata.spring.boot_security.demo.services.AdminService;
 
 @Controller
 public class UsersController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public UsersController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     //попробуем получить данные аутетифицированного пользователя
     //из браузера по кукам достанется объект Principal
@@ -26,6 +35,7 @@ public class UsersController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/AdminService.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')") //условие при котором пускаем пользователя в этот метод
+    public void doAdminStuff(){
+        System.out.println("Only Admin can do this");
+    }
+}
